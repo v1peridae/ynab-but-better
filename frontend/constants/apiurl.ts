300eb57a288e65f60eca85955710ca78a80c40e9
louisa :3 <louisakavindu@gmail.com>
2025-07-22 13:03:24 -0400

    grrrr

--- frontend/constants/apiurl.ts
+++ frontend/constants/apiurl.ts
@@ -3,13 +3,13 @@ import Constants from "expo-constants";
 
 const getApiUrl = () => {
   const hostUri = Constants.expoConfig?.hostUri;
-  const hostname = hostUri?.split(':')[0];
+  const hostname = hostUri?.split(":")[0];
 
   if (hostname) {
     return `http://${hostname}:3000`;
   }
-  
-  return "http://192.168.100.3:3000";
+
+  return "http://165.232.130.152:3000";
 };
 
 const apiUrl = getApiUrl();
