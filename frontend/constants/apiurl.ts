475a6f8ba097c1560b2bae326c10678c1e1ed361
louisa :3 <louisakavindu@gmail.com>
2025-07-28 06:01:35 -0700

    i might lowk js be dumb

--- frontend/constants/apiurl.ts
+++ frontend/constants/apiurl.ts
@@ -1,24 +1,2 @@
-import { Platform } from "react-native";
-import Constants from "expo-constants";
-
-const getApiUrl = () => {
-  const hostUri = Constants.expoConfig?.hostUri;
-  const hostname = hostUri?.split(":")[0];
-
-  if (hostname) {
-    return `http://${hostname}:3000`;
-  }
-
-  return "http://165.232.130.152:3000";
-};
-
-const apiUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://ynab-but-better-production.up.railway.app"
-    : (() => {
-        const hostUri = Constants.expoConfig?.hostUri;
-        const hostname = hostUri?.split(":")[0];
-        return hostname ? `http://${hostname}:3000` : "http://localhost:3000";
-      })();
-
+const apiUrl = "https://ynab-but-better.onrender.com";
 export const API_URL = apiUrl;
